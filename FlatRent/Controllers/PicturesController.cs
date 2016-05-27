aedca7b287ad6f72f5dc596c94a568b35b6a1455
MikeZhuravok <zhuravok96@gmail.com>
2016-05-27 20:15:43 +0300

    working images upload to folder UploadImages

--- FlatRent/Controllers/PicturesController.cs
+++ FlatRent/Controllers/PicturesController.cs
@@ -14,14 +14,12 @@ namespace FlatRent.Controllers
     public class PicturesController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
-        // GET: api/Pictures
+        
         public IQueryable<Picture> GetPictures()
         {
             return db.Pictures;
         }
 
-        // GET: api/Pictures/5
         [ResponseType(typeof(Picture))]
         public IHttpActionResult GetPicture(int id)
         {
@@ -34,55 +32,24 @@ namespace FlatRent.Controllers
             return Ok(picture);
         }
 
-        //[ResponseType(typeof(Picture))]
-        //public IHttpActionResult PostPicture(/*[FromBody]string name,*/ HttpPostedFileBase uploadImage)
-        //{
-
-        //    Picture picture = new Picture()
-        //    {
-        //        Name = /*name*/"random name"
-        //    };
-        //    if (!ModelState.IsValid)
-        //    {
-        //        byte[] imageData = null;
-        //        using (var binaryReader = new BinaryReader(uploadImage.InputStream))
-        //        {
-        //            imageData = binaryReader.ReadBytes(uploadImage.ContentLength);
-        //        }
-        //        picture.Image = imageData;
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    db.Pictures.Add(picture);
-        //    db.SaveChanges();
-
-        //    return CreatedAtRoute("DefaultApi", new { id = picture.ID }, picture);
-        //}
-
-        public IHttpActionResult PostPicture(/*[FromBody]string name,*/ HttpPostedFileBase uploadImage)
+        public IHttpActionResult PostPicture()
         {
-            if (uploadImage != null)
+            if (HttpContext.Current.Request.Files.AllKeys.Any())
             {
-                string pic = System.IO.Path.GetFileName(uploadImage.FileName);
-                string path = System.IO.Path.Combine(
-                                       System.Web.HttpContext.Current.Server.MapPath("~/images/profile"), pic);
-                // file is uploaded
-                uploadImage.SaveAs(path);
+                var httpPostedFile = HttpContext.Current.Request.Files["uploadImage"];
 
-                // save the image path path to the database or you can send image 
-                // directly to database
-                // in-case if you want to store byte[] ie. for DB
-                using (MemoryStream ms = new MemoryStream())
+                if (httpPostedFile != null)
                 {
-                    uploadImage.InputStream.CopyTo(ms);
-                    byte[] array = ms.GetBuffer();
+                    var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/UploadedFiles"), httpPostedFile.FileName);
+
+                    httpPostedFile.SaveAs(fileSavePath);
                 }
             }
 
             return CreatedAtRoute("DefaultApi", new { id = 1 }, new Picture { ID = 1});
         }
        
-        // DELETE: api/Pictures/5
+
         [ResponseType(typeof(Picture))]
         public IHttpActionResult DeletePicture(int id)
         {
